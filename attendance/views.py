ee97a3036c2ba9cb37445473306eef1345fb443d
Zhaolin99 <78530943+Zhaolin99@users.noreply.github.com>
2025-05-01 10:36:42 -0700

    Update views.py
    Update Attendance/Performance superuser permission

--- attendance/views.py
+++ attendance/views.py
@@ -1,13 +1,24 @@
 from rest_framework import viewsets, permissions
 from .models import Attendance, Performance
 from .serializers import AttendanceSerializer, PerformanceSerializer
+from employees.permissions import IsAdmin, IsHR, IsEmployee
 
 class AttendanceViewSet(viewsets.ModelViewSet):
     queryset = Attendance.objects.all()
     serializer_class = AttendanceSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAuthenticated(), (IsAdmin() or IsHR())]
+        return [permissions.IsAuthenticated()]
+
 
 class PerformanceViewSet(viewsets.ModelViewSet):
     queryset = Performance.objects.all()
     serializer_class = PerformanceSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [permissions.IsAuthenticated(), (IsAdmin() or IsHR())]
+        return [permissions.IsAuthenticated()]
