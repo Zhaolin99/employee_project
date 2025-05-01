13db65df42c2eb1ba0278a234fa29bdce71ddbff
Zhaolin99 <78530943+Zhaolin99@users.noreply.github.com>
2025-05-01 10:30:55 -0700

    Update permissions.py
    Update superuser permission

--- employees/permissions.py
+++ employees/permissions.py
@@ -2,12 +2,24 @@ from rest_framework.permissions import BasePermission
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'Admin'
+        return (
+            request.user and
+            request.user.is_authenticated and
+            (request.user.is_superuser or request.user.role == 'Admin')
+        )
 
 class IsHR(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'HR'
+        return (
+            request.user and
+            request.user.is_authenticated and
+            (request.user.is_superuser or request.user.role == 'HR')
+        )
 
 class IsEmployee(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'Employee'
+        return (
+            request.user and
+            request.user.is_authenticated and
+            (request.user.is_superuser or request.user.role == 'Employee')
+        )
